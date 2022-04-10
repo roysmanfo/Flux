e0db11bb28d78203f04d12f9ee2d8ca2138dfc4d
RoysManfo <manforoys@gmail.com>
2022-04-10 13:15:02 +0200

    countextension

---
+++ source/cmds/countextension.py
@@ -0,0 +1,29 @@
+#Dato un path di un file, restituisce il numero di file con estensione specificata.
+#esempio:
+#input: .py
+#percorso: C:\Users\User\Desktop\python\test
+#output: Sono presenti 81989 file .py nella cartella: C:\Users\User\Desktop\python\test
+#se non viene fornito il path, viene utilizzato il percorso corrente
+
+def count_extension(extension, pathToFile=None):
+    try:
+        import os
+        from colorama import Fore
+        if pathToFile is None or pathToFile == "":
+            pathToFile = os.getcwd()
+        count = 0
+        for file in os.listdir(pathToFile):
+            if file.endswith(extension):
+                count += 1
+        print(f'{Fore.WHITE}Sono presenti {count} file {extension} nella cartella: {pathToFile}')
+    
+    except OSError:
+        #Se il path specificato non esiste verrà restituito un errore 
+        print(f'{Fore.RED}Il percorso specificato non esiste')
+        restart = input(f'{Fore.WHITE}Vuoi ripetere l\'operazione? (y/n):  {Fore.BLUE}')
+        if restart == 'y':
+            newPathToFile = input(f'{Fore.WHITE}Inserisci il nuovo percorso:  {Fore.BLUE}')
+            count_extension(extension, newPathToFile)
+        else:
+            print(f'{Fore.RED}Operazione annullata{Fore.WHITE}')
+        
