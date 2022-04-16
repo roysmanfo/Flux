d7bffa57be2604bb454e47c1eeca06cbaf62aa70
RoysManfo <manforoys@gmail.com>
2022-04-16 18:57:45 +0200

    Fix: risolti errori al digitare di "countfile"

--- source/cmds/countfile.py
+++ source/cmds/countfile.py
@@ -4,12 +4,16 @@ import os
 from colorama import Fore
 
 def run(cmd):
-    if cmd[1] == '--HELP' or cmd[1] == '--H':
+    if len(cmd) < 2 :
+        error('Syntax Error')
+
+    elif cmd[1] == '--HELP' or cmd[1] == '--H':
         help()
+    
     else:
         execute(cmd)
 
-def count_extension(extension, pathToFile=None):
+def count_extension(extension:str, pathToFile:str=None) -> None:
     try:
         
         if pathToFile is None or pathToFile == "":
@@ -21,6 +25,10 @@ def count_extension(extension, pathToFile=None):
         print(f'{Fore.WHITE}Sono presenti {count} file {extension} nella cartella: {pathToFile}')
     
     except OSError:
+        error('Path Error',extension)
+
+def error(problem:str, extension:str = None) -> None:
+    if problem == 'Path Error':
         #Se il path specificato non esiste verrà restituito un errore 
         print(f'{Fore.RED}Il percorso specificato non esiste')
         restart = input(f'{Fore.WHITE}Vuoi ripetere l\'operazione? (y/n):  {Fore.BLUE}')
@@ -30,8 +38,11 @@ def count_extension(extension, pathToFile=None):
             count_extension(extension, newPathToFile)
         else:
             print(f'{Fore.RED}Operazione annullata{Fore.WHITE}')
+    elif problem == 'Syntax Error':
+        #Se la sintassi non è corretta verrà restituito un errore
+        print(f'{Fore.RED}Errore: estensione mancante{Fore.RESET}')
 
-def execute(cmd):
+def execute(cmd:list) -> None:
     extension = cmd[1].lower()
     try:
         pathToFile = cmd[2]
@@ -41,7 +52,7 @@ def execute(cmd):
         pathToFile = None
     count_extension(extension, pathToFile)
 
-def help():
+def help() -> None:
     print(f'{Fore.MAGENTA}COUNTFILE')
     print(f'\n{Fore.WHITE}Questo comando permette di contare il numero di file con una determinata estensione')
     print(f'{Fore.WHITE}Sintassi: countfile <estensione> <percorso>')
