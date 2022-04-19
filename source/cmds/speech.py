9cbe89cfc8756fbe23ca0b5861ec538308ee7721
RoysManfo <manforoys@gmail.com>
2022-04-19 23:35:22 +0200

    Feature: speech --audio  --file

--- source/cmds/speech.py
+++ source/cmds/speech.py
@@ -3,18 +3,26 @@ import speech_recognition as sr
 import os
 
 
-# Sintassi: speech --audio <lang> <name> --file <filePath> <FileDestination>    Conversione file
+# Sintassi: speech --audio <lang> <name> --file  <filePath> <FileDestination>    Conversione file
 #           speech --audio <text> <FileDestination> <lang>                      Conversione testo   
 
 # Sintassi: speech --text <lang> <name> --file <FileDestination>                Conversione file
 #           speech --text <lang> <name> <FileDestination>                       Conversione testo
 
 def run(cmd):
-    if '--audio' in cmd and '--text' not in cmd:
-        if '--file' in cmd:
+    if cmd[0] == '--AUDIO' and '--TEXT' not in cmd:
+        if '--FILE' in cmd:
             # .txt => .mp3
-            file = cmd[cmd.index('--file') + 1]
-            print('file: ' + file)
+            lang = cmd[cmd.index('--AUDIO') + 1]
+            name = cmd[cmd.index('--AUDIO') + 2]
+            filePath = cmd[cmd.index('--FILE') + 1]
+            fileDestination = cmd[cmd.index('--FILE') + 2]
+            text = open(filePath, 'r').read()
+
+            TextToSpeech.convert(text, lang, name, fileDestination)
+        elif '--FILE' not in cmd:
+            # text => .mp3
+            pass
             
 
 class TextToSpeech:
