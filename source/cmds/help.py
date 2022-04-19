046287c88fb1463a1dfdd8a4c1d1ca05f79997f5
RoysManfo <manforoys@gmail.com>
2022-04-19 22:24:13 +0200

    Fix: Aggiunte indicazioni

--- source/cmds/help.py
+++ source/cmds/help.py
@@ -1,18 +1,20 @@
 import json
 from colorama import Fore
 class General:
-    def help():
+    def help() -> None:
         import os
         cDir = f'{os.getcwd()}\\cmds\\files\\'
         with open(f'{cDir}\\commands.json','r') as file:
             l = json.load(file)
+            print(f'{Fore.CYAN}\nComandi disponibili:\n')
             for i in l:
                 print(f'{Fore.WHITE + l[i].upper()}')
+            print('\nPer ulteriori informazioni su uno specifico comando, digitare "nome_comando --help"')
     
-    def help_detailed():
+    def help_detailed() -> None:
         #Qui verranno date informazioni sulla sintassi dei comandi
         pass
 
-class Specific:
-    # Diverse funzioni spiegeranno come usare determinati comandi, e a cosa servono con degli esempi visivi
+class Hidden:
+    # comandi nascosti
     pass
