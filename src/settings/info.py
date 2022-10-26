4102585f0304f36a791d2638fc96904b3fe4ef9f
RoysManfo <manforoys@gmail.com>
2022-10-26 23:57:54 +0200

    Added some documentation for the class User

--- src/settings/info.py
+++ src/settings/info.py
@@ -1,6 +1,24 @@
 import json, os, platform
 
 class User():
+    """
+    ### CLASS USER
+    The class User manages all user info and settings, plus this
+    approach makes working with user properties much easier than in
+    the previus version of Cristal, where all settings where also stored
+    in a single json file like in this version, but to access it it wes
+    neccessary to pass multiple parameters to a function, which will pass
+    them to anoather function and make refactoring much harder.
+
+    By using an instance of this class, we need to just pass the current
+    user istance (USER) and accessing its settings like with any normal
+    python object:
+    
+    Example: 
+    ```py
+    send_email(sender=USER.email, context=..., ...)
+    ```
+    """
     def __init__(self):
         with open("./settings.json") as f:
             sett = json.load(f)
