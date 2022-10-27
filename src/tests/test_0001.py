6cbc63b7f383e81c9cf59e2b7ef92892aa7d944d
RoysManfo <manforoys@gmail.com>
2022-10-27 22:18:53 +0200

    Writing some tests

---
+++ src/tests/test_0001.py
@@ -0,0 +1,21 @@
+import unittest, sys
+
+class Test_TestUser(unittest.TestCase):
+    def test_user_has_all_attributes(self):
+        from src.settings.info import User
+        USER = User()
+        types = [
+            type(USER.email, str),
+            type(USER.language, str),
+            type(USER.language_audio, str),
+            type(USER.language_text, str),
+            type(USER.paths, dict),
+        ]
+        result = all(types)
+        self.assertTrue(result)
+
+    def test_decrement(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
