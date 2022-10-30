9c18c214e353f25d35da46dfe008d5f64acd038a
RoysManfo <manforoys@gmail.com>
2022-10-30 11:00:34 +0100

    Resolved problem with a test

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,15 +1,15 @@
 import unittest
 
 class Test_TestUser(unittest.TestCase):
-    def test_user_has_all_attributes(self):
-        from src.settings.info import User
+    def test_user_has_correct_attributes(self):
+        from src.settings.info import User, Path
         USER = User()
         types = [
             type(USER.email) == str,
             type(USER.language) == str,
             type(USER.language_audio) == str,
             type(USER.language_text) == str,
-            type(USER.paths) == dict,
+            isinstance(USER.paths, type(Path))
         ]
         result = all(types)
         self.assertTrue(result)
