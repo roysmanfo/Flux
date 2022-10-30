b6e71f35a46fbb2fadd721eb730a9bf60c1b254c
RoysManfo <manforoys@gmail.com>
2022-10-31 00:30:53 +0100

    test added

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -11,7 +11,18 @@ class Test_TestUtils(unittest.TestCase):
         test = "   Hello   World!   "
         formatted = transform.string_to_list(test)
         ERROR_MSG = "command.string_to_list() should return a list with distributed elements andheading and trailing spaces removed"
-        self.assertEqual(formatted, ["Hello", "World!"], ERROR_MSG)
+        self.assertFalse(formatted == ["Hello", "World!"], ERROR_MSG)
+    
+    def test_returns_a_list_2(self):
+        """
+        Check if the command.py util works properly
+        """
+        from src.utils import transform
+
+        test = "   Hello   World!   "
+        formatted = transform.string_to_list(test)
+        ERROR_MSG = "command.string_to_list() should return a list with distributed elements andheading and trailing spaces removed"
+        self.assertTrue(formatted == ["HELLO", "WORLD!"], ERROR_MSG)
         
 
 if __name__ == '__main__':
