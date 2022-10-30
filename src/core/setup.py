8c227efdfe66bf338b8dcefa2fc67fd6acb6bff3
RoysManfo <manforoys@gmail.com>
2022-10-30 22:33:57 +0100

    Moved setup file

--- src/core/setup.py
+++ src/core/setup.py
@@ -1,17 +1,18 @@
-def setup():
+def setup(user, path) -> tuple:
     """
     ## Setup process
-    
+
     This process retrives all data necessary to start using Cristal, like
     User setings, file locations on the disk, etc.
 
     This process makes it easier to start Cristal as we just need to call this
     function in the main file and everything will be handled automaticly. 
+
+    @param user : The user class from the info file in settings
+    @param path : The path class from the info file in settings
     """
-    from settings.info import User, Path
-    
-    USER = User()
-    PATH = Path(USER, load_data=True)
 
+    USER = user()
+    PATH = path(USER, load_data=True)
 
-setup()
+    return (USER, PATH)
