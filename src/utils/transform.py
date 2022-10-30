52c08d05df1ab239c1cfbeaade055ca27021b09b
RoysManfo <manforoys@gmail.com>
2022-10-30 23:53:32 +0100

    utils

---
+++ src/utils/transform.py
@@ -0,0 +1,23 @@
+
+def string_to_list(string: str):
+    string.strip()
+    if string == None or string == '':
+        string = ['']
+        return string
+    else:
+        return decode(string)
+
+
+def decode(string: str = None):
+    words = []
+    for i in string:
+        if i == ' ':
+            words.append(string[:string.index(i)])
+            string = string[string.index(i)+1:]
+    words.append(string)
+    length = len(words)
+    while length > 0:
+        if words[length-1] == '':
+            words.pop(length-1)
+        length -= 1
+    return words
