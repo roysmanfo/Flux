5d3c5861410f9745a87e1bd8cd9fec11201b50c6
RoysManfo <manforoys@gmail.com>
2022-10-30 23:54:14 +0100

    modified output from listen()

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
+# Dependencies
 import os
 from colorama import init, Fore
 init(autoreset=True)
 
+# Cristal modules
 from core import setup
 from settings.info import User, Path
+from utils import transform
 
+# Setup process
 setup_result = setup.setup(user=User, path=Path)
 
 USER: User = setup_result[0]
 PATH: Path = setup_result[1]
 VERSION: str = setup_result[2]
 
-def listen():
+def listen() -> list:
     """
     This function is used to get the command typed by the user preceded by
     a string of text containing the username, the name of the program,
@@ -19,6 +23,7 @@ def listen():
     """
     print(f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}{PATH.terminal}{Fore.WHITE} ", end="")
     command = input(f"$ ")
-    return command
+    return transform.string_to_list(command)
 
+# Actual start
 listen()
