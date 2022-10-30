165135fb8d80d33221240f30932c629f756b726c
RoysManfo <manforoys@gmail.com>
2022-10-30 11:07:14 +0100

    Brief description

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,7 +1,12 @@
 import unittest
 
 class Test_TestUser(unittest.TestCase):
+
     def test_user_has_correct_attributes(self):
+        """
+        Check if all attributes heve the desired type
+        """
+
         from src.settings.info import User, Path
         USER = User()
         types = [
