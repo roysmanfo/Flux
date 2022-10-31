fb01eb0f47d18b393d9411c2ff56e9246be67507
RoysManfo <manforoys@gmail.com>
2022-10-31 19:36:15 +0100

    Run function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 # Dependencies
+from utils import transform, builtincmds
+from settings.info import User, Path, SETTINGS_FILE, SETTINGS_FOLDER, VERSION
+from core import setup, manager
 import os
+import platform
 from colorama import init, Fore
 init(autoreset=True)
 
 # Cristal modules
-from core import setup, manager
-from settings.info import User, Path, SETTINGS_FILE, SETTINGS_FOLDER, VERSION
-from utils import transform
 
+# These commands will be executed from the default OS terminal (es. cmd.exe on windows)
+if platform.system() == "Windows":
+    SYSTEM_CMDS = builtincmds.WINDOWS
+elif platform.system() in ["Linux", "Mac"]:
+    SYSTEM_CMDS = builtincmds.LINUX
 
 # Setup process
 setup_result = setup.setup(user=User, path=Path)
@@ -21,9 +27,33 @@ def listen() -> list:
     a string of text containing the username, the name of the program,
     the version and the location where Cristal is oparating on the disk.
     """
-    print(f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}{PATH.terminal}{Fore.WHITE} ", end="")
+    print(
+        f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}{PATH.terminal}{Fore.WHITE} ", end="")
     command = input(f"$ {Fore.MAGENTA}")
+    print(f"{Fore.WHITE}", end="")
+
     return transform.string_to_list(command)
 
-# Actual start
-print(listen())
+
+def run():
+    while True:
+        cmd = listen()
+        if cmd[0] == "exit":
+            os._exit(0)
+
+        # Check if it's a command the default terminal can handle
+        elif cmd[0] in SYSTEM_CMDS:
+            if cmd[0] == "cd" and len(cmd) > 1:
+                os.chdir(cmd[1])
+                PATH.terminal = os.getcwd()
+            else:
+                os.system(f"cd {PATH.terminal} && "+"".join(cmd))
+
+        # Otherwise it might be a Cristal command
+        elif cmd[0] == "cr":
+            cmd.pop(0)
+            manager.manage(cmd)
+
+
+if __name__ == "__main__":
+    run()
