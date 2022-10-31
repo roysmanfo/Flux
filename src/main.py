dc2c06506655aac210c9bd5d459d70797e89f277
RoysManfo <manforoys@gmail.com>
2022-11-01 00:38:27 +0100

    Converted backslashes with fore ones

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ setup_result = setup.setup(user=User, path=Path)
 USER: User = setup_result[0]
 PATH: Path = setup_result[1]
 
+
 def listen() -> list:
     """
     This function is used to get the command typed by the user preceded by
@@ -28,7 +29,7 @@ def listen() -> list:
     the version and the location where Cristal is oparating on the disk.
     """
     print(
-        f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}{PATH.terminal}{Fore.WHITE}{Fore.MAGENTA} $ ", end="")
+        f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}" + PATH.terminal.replace("\\", "/") + f"{Fore.WHITE}{Fore.MAGENTA} $ ", end="")
     command = input()
     print(f"{Fore.WHITE}", end="")
 
@@ -40,7 +41,7 @@ def run():
         os.chdir(PATH.terminal)
         cmd = listen()
 
-        # Check if we just want to leave, there's no need to check in  
+        # Check if we just want to leave, there's no need to check in
         # all of SYSTEM_CMDS if we just want to leave and do it quickly
         if cmd[0] == "exit":
             os._exit(0)
@@ -57,10 +58,11 @@ def run():
         elif cmd[0] == "cr":
             cmd.pop(0)
             manager.manage(cmd)
-        
+
         # Command not found, a message will be displayed based on USER.language
         else:
             pass
+
 
 if __name__ == "__main__":
     run()
