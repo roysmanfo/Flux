2e2deed6c94cc2d274e94ff65fe977b05888ea85
RoysManfo <manforoys@gmail.com>
2022-11-01 16:38:31 +0100

    command `cr`

--- src/main.py
+++ src/main.py
@@ -2,24 +2,31 @@
 from utils import transform, builtincmds
 from settings.info import User, Path, SETTINGS_FILE, SETTINGS_FOLDER, VERSION
 from core import setup, manager
+from core.cmd import cr
 import os
 import platform
 from colorama import init, Fore
 init(autoreset=True)
-
 # Cristal modules
 
-# These commands will be executed from the default OS terminal (es. cmd.exe on windows)
-if platform.system() == "Windows":
-    SYSTEM_CMDS = builtincmds.WINDOWS
-elif platform.system() in ["Linux", "Mac"]:
-    SYSTEM_CMDS = builtincmds.LINUX
 
 # Setup process
 setup_result = setup.setup(user=User, path=Path)
-
 USER: User = setup_result[0]
 PATH: Path = setup_result[1]
+
+
+
+if platform.system() == "Windows":
+    SYSTEM_CMDS = builtincmds.WINDOWS # These commands will be executed from the default OS terminal (cmd.exe)
+    LANG_FILE: str = os.path.join(os.path.dirname(__file__)+"\\lang\\", USER.language + ".txt")
+
+elif platform.system() in ["Linux", "Mac"]:
+    SYSTEM_CMDS = builtincmds.LINUX # These commands will be executed from the default OS terminal (linux terminal)
+    LANG_FILE: str = os.path.join(os.path.realpath(__file__)+"/lang/", USER.language + ".txt")
+
+
+
 
 
 def listen() -> list:
@@ -29,7 +36,7 @@ def listen() -> list:
     the version and the location where Cristal is oparating on the disk.
     """
     print(
-        f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}" + PATH.terminal.replace("\\", "/").lower() + f"{Fore.WHITE}{Fore.MAGENTA} $ ", end="")
+        f"{Fore.GREEN}{USER.username}{Fore.CYAN} Cristal [{VERSION}] {Fore.YELLOW}" + PATH.terminal.lower() + f"{Fore.WHITE}{Fore.MAGENTA} $ ", end="")
     command = input()
     print(f"{Fore.WHITE}", end="")
 
@@ -51,6 +58,9 @@ def run():
             if cmd[0] == "cd" and len(cmd) > 1:
                 os.chdir(cmd[1])
                 PATH.terminal = os.getcwd()
+                
+                if platform.system() == "Windows":
+                    PATH.terminal.replace("/","\\")
             else:
                 os.system(f"cd {PATH.terminal} && "+" ".join(cmd))
 
@@ -60,8 +70,7 @@ def run():
                 cmd.pop(0)
                 manager.manage(cmd)
             else:
-                # Informations about the programm
-                pass
+                cr.run(USER, LANG_FILE)
 
         # Command not found, a message will be displayed based on USER.language
         else:
