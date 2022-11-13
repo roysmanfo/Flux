fbc079557e6cc1b65e8d08d0b4dc76d19d609cb0
RoysManfo <manforoys@gmail.com>
2022-11-13 02:16:26 +0100

    Fixes on command observer

--- src/core/cmd/observer.py
+++ src/core/cmd/observer.py
@@ -30,7 +30,7 @@ SOFTWARE.
 """
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileMovedEvent
+from watchdog.events import FileSystemEventHandler
 import os, shutil
 from datetime import date
 from pathlib import Path
@@ -39,36 +39,51 @@ from time import sleep
 
 def run(info: list):
     print("Observer running")
-    print(info[1].bucket, info[1].bucket_destination)
+    print("Press Ctrl+C to stop")
     sort_files(info)
     print("Observer stopped")
 
 def sort_files(info: list):
     watch_path = Path(info[1].bucket)
     destination_root = Path(info[1].bucket_destination)
-    event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
-
-    observer = Observer()
-    observer.schedule(event_handler, f'{watch_path}', recursive=True)
-    observer.start()
-
+    
     try:
-        while True:
-            sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        os.makedirs(watch_path) 
+    except FileExistsError:
+        pass
+    
+    try:
+        os.makedirs(destination_root) 
+    except FileExistsError:
+        pass
+    
+    try:
+        event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
 
-def add_date_to_path(path: Path):
+        observer = Observer()
+        observer.schedule(event_handler, f'{watch_path}', recursive=True)
+        observer.start()
+
+        try:
+            while True:
+                # sleep(.1)
+                continue
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+        
+    except FileNotFoundError:
+        # We deleted one or both directories
+        sort_files(info)
+
+def create_destination_path(path: Path):
     """
     Helper function that adds current year/month to destination path. If the path
     doesn't already exist, it is created.
     :param Path path: destination root to append subdirectories based on date
     """
-    dated_path = path / f'{date.today().year}' / f'{date.today().month:02d}'
-    dated_path.mkdir(parents=True, exist_ok=True)
-    return dated_path
-
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 def rename_file(source: Path, destination_path: Path):
     """
@@ -96,12 +111,18 @@ class EventHandler(FileSystemEventHandler):
         self.watch_path = watch_path.resolve()
         self.destination_root = destination_root.resolve()
 
-    def on_modified(self, event=FileMovedEvent):
-        print("File detected")
+    def on_modified(self, event):
         for child in self.watch_path.iterdir():
+            
             # skips directories and non-specified extensions
             if child.is_file() and child.suffix.lower() in extension_paths:
                 destination_path = self.destination_root / extension_paths[child.suffix.lower()]
-                destination_path = add_date_to_path(path=destination_path)
+                destination_path = create_destination_path(path=destination_path)
+                destination_path = rename_file(source=child, destination_path=destination_path)
+                shutil.move(src=child, dst=destination_path)
+            
+            elif child.is_file() and child.suffix.lower() not in extension_paths:
+                destination_path = self.destination_root / extension_paths["noname"]
+                destination_path = create_destination_path(path=destination_path)
                 destination_path = rename_file(source=child, destination_path=destination_path)
                 shutil.move(src=child, dst=destination_path)
