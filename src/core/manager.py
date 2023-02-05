b148e6ec2fef93dc0125250b9b5efcfd5fe7d2af
RoysManfo <manforoys@gmail.com>
2023-02-05 20:50:44 +0100

    removed `async` and `await`

--- src/core/manager.py
+++ src/core/manager.py
@@ -43,13 +43,13 @@ def classify_arguments(command: list) -> dict:
     return classified
 
 
-async def manage(cmd: list, info: object) -> None:
+def manage(cmd: list, info: object) -> None:
 
     # Classify command line arguments and send them to be analized
-    await switch(classify_arguments(cmd), info)
+    switch(classify_arguments(cmd), info)
 
 
-async def switch(command: dict, info: object) -> None:
+def switch(command: dict, info: object) -> None:
 
     # Match the command name to the corresponding file in ./cmd/
     # for further processing and execution
@@ -57,7 +57,7 @@ async def switch(command: dict, info: object) -> None:
     from . import cmd as cr
 
     if command["command"] == "observer":
-        await cr.observer.run(command = command, info = info, from_command_line=True)
+        cr.observer.run(command = command, info = info, from_command_line=True)
 
     if command["command"] == "set":
         cr.set.run(command = command, info = info)
