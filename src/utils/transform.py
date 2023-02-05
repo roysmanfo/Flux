32963e2ca1be0baafa3f6cba5a7edd248634cee2
RoysManfo <manforoys@gmail.com>
2023-02-05 23:42:03 +0100

    Simplified function

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -1,30 +1,4 @@
 
-def string_to_list(string: str):
-    string.strip()
-    if string == None or string == '':
-        return ['']
-    else:
-        return decode(string)
-
-
-def decode(string: str = "") -> list[str]:
-    words = []
-    for i in string:
-        if i == ' ':
-            words.append(string[:string.index(i)])
-            string = string[string.index(i)+1:]
-    words.append(string)
-    length = len(words)
-    while length > 0:
-        if words[length-1] == '':
-            words.pop(length-1)
-        length -= 1
-    words = [i for i in words]
-    words[0].upper()
-    
-    try:
-        words[1].upper()
-    except IndexError:
-        pass
-
-    return words
+def string_to_list(string: str) -> list[str]:
+    words = string.strip().split(" ")
+    return [i.lower() for i in words]
