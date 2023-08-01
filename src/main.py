0d713d257d93067d84289aca7770266d1e9a52e5
RoysManfo <manforoys@gmail.com>
2023-08-02 00:19:38 +0200

    Simplify code and reorganize imports

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-# Dependencies
-from threading import Thread
-from utils import transform
-from settings.info import User, Info, SETTINGS_FILE, SETTINGS_FOLDER
-from core import setup, manager
-from core.cmd import cr
-import os
-from colorama import init, Fore
-import subprocess
-import platform
-import sys
-init(autoreset=True)
 # Cristal modules
+from core.cmd import cr
+from core import setup, manager
+from settings.info import User, Info, SETTINGS_FILE, SETTINGS_FOLDER
+from utils import transform
+
+# External Dependencies
+import sys
+import os
+import subprocess
+from threading import Thread
+from colorama import init, Fore
+init(autoreset=True)
 
 
 # Setup process
@@ -100,8 +100,7 @@ def default_terminal_output(command: str) -> str | int:
     """
 
     if command.rstrip('\n') in ['cls', 'clear']:
-        os.system('cls') if platform.system(
-        ) == 'Windows' else os.system('clear')
+        os.system('cls || clear')
         return "\r"
 
     pipe = subprocess.run(
