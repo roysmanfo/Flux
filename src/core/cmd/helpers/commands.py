efff1fa64e81456d4fb1116e3fcde3069eaf67a7
RoysManfo <manforoys@gmail.com>
2023-08-04 02:08:34 +0200

    Add default messages

--- src/core/cmd/helpers/commands.py
+++ src/core/cmd/helpers/commands.py
@@ -9,13 +9,21 @@ class CommandInterface:
     
     ### FUNCTIONS
 
-    - `init()` This is function is called right before run(). This function should be used to do setup operations   
-    - `run()`  This is the entry function for the command. This function should be used to manage arguments   
+    - `init()` This is function is called right before run().   
+    - `run()`  This is the entry function for the command.   
 
     """
 
     def init(self, *args, **kwargs):
+        """
+        This is function is called right before run().\n
+        This function should be used to do setup operations
+        """
         ...
     
     def run(self, command: list[str], info: object, *args, **kwargs):
+        """
+        This is the entry function for the command.\n
+        This function should be used to manage arguments and adapt command execution.
+        """
         ...
