30507e969f276517365e5d64e969f57294ca4ac9
RoysManfo <manforoys@gmail.com>
2023-08-07 23:47:52 +0200

    Updated tests

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -10,13 +10,13 @@ class Test_Variables(unittest.TestCase):
 
         from src.core import setup
         from src.settings.info import Info, User, SETTINGS_FILE, SETTINGS_FOLDER
-        from src.core.cmd import set
+        from src.core.cmd import export
 
         INFO: Info = setup.setup(User, Info, SETTINGS_FILE, SETTINGS_FOLDER)
-        result = set.set_user_variable({"variables": ['foo', 'bar']}, INFO)
-
-        self.assertFalse(
-            result, "The variable has been created, but it wasn't supposed to")
+        cmnd = export.Command()
+        cmnd.run(['export', 'foo', 'bar'], INFO)
+        result = INFO.variables.get("foo", None)
+        self.assertTrue(result == None, "The variable has been created, but it wasn't supposed to")
 
     def test_variables_02(self):
         """
@@ -25,13 +25,13 @@ class Test_Variables(unittest.TestCase):
 
         from src.core import setup
         from src.settings.info import Info, User, SETTINGS_FILE, SETTINGS_FOLDER
-        from src.core.cmd import set
+        from src.core.cmd import export
 
         INFO: Info = setup.setup(User, Info, SETTINGS_FILE, SETTINGS_FOLDER)
-        result = set.set_user_variable({"variables": ['$foo']}, INFO)
-
-        self.assertFalse(
-            result, "The variable has been created, but it wasn't supposed to")
+        cmnd = export.Command()
+        cmnd.run(['export', '$foo'], INFO)
+        result = INFO.variables.get("foo", None)
+        self.assertTrue(result == None, "The variable has been created, but it wasn't supposed to")
 
     def test_variables_03(self):
         """
@@ -40,28 +40,13 @@ class Test_Variables(unittest.TestCase):
 
         from src.core import setup
         from src.settings.info import Info, User, SETTINGS_FILE, SETTINGS_FOLDER
-        from src.core.cmd import set
+        from src.core.cmd import export
 
         INFO: Info = setup.setup(User, Info, SETTINGS_FILE, SETTINGS_FOLDER)
-        result = set.set_user_variable({"variables": ['$foo']}, INFO)
-
-        self.assertFalse(
-            result, "The variable has been created, but it wasn't supposed to")
-
-    def test_variables_04(self):
-        """
-        Spelling variable syntax
-        """
-
-        from src.core import setup
-        from src.settings.info import Info, User, SETTINGS_FILE, SETTINGS_FOLDER
-        from src.core.cmd import set
-
-        INFO: Info = setup.setup(User, Info, SETTINGS_FILE, SETTINGS_FOLDER)
-        result = set.set_user_variable({"variables": ['$', 'bar']}, INFO)
-
-        self.assertFalse(
-            result, "The variable has been created, but it wasn't supposed to")
+        cmnd = export.Command()
+        cmnd.run(['export', '$$', 'bar'], INFO)
+        result = INFO.variables.get("$", None)
+        self.assertTrue(result == None, "The variable has been created, but it wasn't supposed to")
 
 
 if __name__ == '__main__':
