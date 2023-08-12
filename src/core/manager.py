fd6bcf15fc0e180286039efb50b8e3d15b103a7e
RoysManfo <manforoys@gmail.com>
2023-08-13 00:28:26 +0200

    removed unnecessary code

--- src/core/manager.py
+++ src/core/manager.py
@@ -6,53 +6,7 @@ with the respective command (if existent).
 """
 from src.settings.info import Info
 
-def classify_arguments(command: list) -> dict:
-    """
-    DEPRECATED
-
-    Classifies the arguments of to the command in a dictionary,
-    divideing each argument in 3 sections:
-     - command :    a string containing the actual command given
-     - flags :      a list of allthe flags that modify how the command is executed (es. -n 3)
-     - variables :  a list of vareables that a flag might require
-     - options :    a list of all the options that modify what the command does
-
-    #### Example
-        input:  ["sortfiles", "/reverse"]
-
-        output: {"command": "sortfiles", "flags": [], "variables": [], "options":["/reverse"]}
-    """
-
-    classified = {"command": "", "flags": [], "variables": [], "options": []}
-
-    options, flags, variables = [], [], []
-
-    for i in command:
-        if command[0] == i:
-            continue
-        if i[0] == "/":
-            options.append(i)
-        elif i[0] == "-":
-            flags.append(i)
-        else:
-            variables.append(i)
-
-    classified["command"] = command[0].lower()
-    classified["flags"] = flags
-    classified["variables"] = variables
-    classified["options"] = options
-
-    return classified
-
-
-def manage(cmd: list[str], info: object) -> None:
-
-    # Classify command line arguments and send them to be analized
-    # switch(classify_arguments(cmd), info)
-    switch(cmd, info)
-
-
-def switch(command: list[str], info: Info) -> None:
+def manage(command: list[str], info: Info) -> None:
 
     # Match the command name to the corresponding file in ./cmd/
     # for further processing and execution
