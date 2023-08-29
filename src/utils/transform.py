7abb77860b8148874fc5f6446bb51ed18fdf42e3
RoysManfo <manforoys@gmail.com>
2023-08-29 22:36:32 +0200

    Edge case

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -7,4 +7,6 @@ def string_to_list(string: str) -> list[str]:
     words = shlex.split(string)
     if len(words) > 0:
         words[0] = words[0].lower() if not words[0].startswith("$") else words[0]
-    return words
+        return words
+    return [""]
+        
