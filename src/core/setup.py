804c9cd2a580e00c10fdea894ff0f9e4fd659c6a
RoysManfo <manforoys@gmail.com>
2023-08-30 11:30:56 +0200

    Remove unnecessary args

--- src/core/setup.py
+++ src/core/setup.py
@@ -2,13 +2,10 @@
 Necessary procedures to prepare the program to work
 as intended.
 """
-from . import bgtasks
-from threading import Thread
-from pathlib import Path
-from os import chdir
+import os
+from src.settings.info import User, Info
 
-
-def setup(user: object, info: object, SETTINGS_FILE: Path, SETTINGS_FOLDER: Path) -> object:
+def setup() -> Info:
     """
     ## Setup process
 
@@ -18,40 +15,15 @@ def setup(user: object, info: object, SETTINGS_FILE: Path, SETTINGS_FOLDER: Path
     This process makes it easier to start Flux as we just need to call this
     function in the main file and everything will be handled automaticaly. 
 
-    Parameters
-    ----------
-    @param user : The user class from the info file in settings/
-    @param info : The info class from the info file in settings/
-    @param SETTINGS_FILE : The SETTINGS_FILE costant from the info file in settings
-    @param SETTINGS_FOLDER : The SETTINGS_FOLDER costant from the info file in settings
-
     Returns
     -------
-    @returns : an object of type Info
+    @returns : An object of type Info
     """
 
     # Load user
-    USER = user()
-    chdir(USER.paths.terminal)
+    USER = User()
+    os.chdir(USER.paths.terminal)
 
-    INFO = info(USER)
+    INFO = Info(USER)
     return INFO
 
-
-
-def get_setup_settings() -> list[str]:
-    import platform
-    import os
-    from dotenv import load_dotenv
-
-    load_dotenv(verbose=False)
-
-    if platform.system() == "Windows":
-        # These commands will be executed from the default OS terminal (cmd.exe)
-        SYSTEM_CMDS = os.getenv('WINDOWS').split(", ")
-
-    elif platform.system() in ["Linux", "Mac"]:
-        # These commands will be executed from the default OS terminal (linux terminal)
-        SYSTEM_CMDS = os.getenv('LINUX').split(", ")
-
-    return SYSTEM_CMDS
