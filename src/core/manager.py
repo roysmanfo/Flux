5c09e6dea7a6d5cdfbcef7163a7c71c38ebe8fe7
RoysManfo <manforoys@gmail.com>
2023-09-02 22:17:36 +0200

    Alow vaieables to store comands to execute

--- src/core/manager.py
+++ src/core/manager.py
@@ -6,6 +6,7 @@ with the respective command (if existent).
 """
 from src.settings.info import Info
 
+
 def manage(command: list[str], info: Info) -> None:
 
     # Match the command name to the corresponding file in ./cmd/
@@ -24,29 +25,39 @@ def manage(command: list[str], info: Info) -> None:
 
     info.processes.clean()
 
-    if command[0] == "export":
+    command_name = info.variables.get(command[0]).value if info.variables.exists(command[0]) else command[0]
+
+    if command[0].startswith("$") and len(command_name.split()) > 1:
+        command.pop(0)
+        command = command_name.split().extend(command)
+        command_name = command[0]
+
+    if command_name == "export":
         exec_command_class = fluxcmd.export.Command
 
-    if command[0] == "file":
+    elif command_name == "file":
         exec_command_class = fluxcmd.file.Command
 
-    elif command[0] == "flux":
+    elif command_name == "flux":
         exec_command_class = fluxcmd.flux.Command
 
-    elif command[0] == "joke":
+    elif command_name == "joke":
         exec_command_class = fluxcmd.joke.Command
-    
-    elif command[0] == "ls":
+
+    elif command_name == "ls":
         exec_command_class = fluxcmd.ls.Command
-    
-    elif command[0] == "observer":
+
+    elif command_name == "observer":
         exec_command_class = fluxcmd.observer.Command
 
-    elif command[0] == "ps":
+    elif command_name == "ps":
         exec_command_class = fluxcmd.ps.Command
 
-    elif command[0] == "systemctl":
+    elif command_name == "systemctl":
         exec_command_class = fluxcmd.systemctl.Commmand
+
+    else:
+        print(f"-flux: {command_name}: command not found\n")
 
     try:
         is_thread = command[-1] == "&"
@@ -59,7 +70,5 @@ def manage(command: list[str], info: Info) -> None:
             execute_command(exec_command, command)
 
     except UnboundLocalError as e:
-        if command[0] != "":
-            print(f"-flux: {command[0]}: command not found\n{e}\n")
-
-
+        if command_name != "":
+            print(f"-flux: {command_name}: command not found\n{e}\n")
