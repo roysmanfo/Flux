35589a1374819b1e3a6bfb8c489257cf14b96228
RoysManfo <manforoys@gmail.com>
2023-09-09 20:13:32 +0200

    New way to return process info

--- src/core/system/processes.py
+++ src/core/system/processes.py
@@ -4,13 +4,37 @@ from typing import List, Callable
 import os
 
 # Process status codes
-STATUS_OK = 0 # Exited with no problems
-STATUS_ERR = 1 # An error accoured
-STATUS_WARN = 2 # Exited with warnings
+STATUS_OK = 0  # Exited with no problems
+STATUS_ERR = 1  # An error accoured
+STATUS_WARN = 2  # Exited with warnings
+
+
+class ProcessInfo:
+    def __init__(self, id: int, owner: str, name: str, native_id: int, time_alive: str, is_reserved_process: bool) -> None:
+        self.id = id
+        self.owner = owner
+        self.name = name
+        self.native_id = native_id
+        self.time_alive = time_alive
+        self.is_reserved_process = is_reserved_process
+
+    def __str__(self) -> str:
+        s = "ProcessInfo("
+        s += f"id={self.id}, "
+        s += f"owner={self.owner}, "
+        s += f"name={self.name}, "
+        s += f"native_id={self.native_id}, "
+        s += f"is_reserved_process={self.is_reserved_process}, "
+        s += f"time_alive={self.time_alive}"
+        s += ")"
+        return s
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
 
 class Process:
-    def __init__(self, id: int, owner: str, command_instance: Callable, line_args: list[str]) -> None:
+    def __init__(self, id: int, owner: str, command_instance: Callable, line_args: list[str], is_reserved_process: bool) -> None:
         self.id: int = id
         self.name: str = line_args[0]
         self.owner: str = owner
@@ -19,6 +43,21 @@ class Process:
         self.started: float = time.time()
         self.status: int | None = None
         self.thread: Thread = None
+        self.native_id: int | None = None
+        self.is_reserved_process = is_reserved_process
+        # self.process_info = ProcessInfo(self.id, self.owner, self.thread.name, self.thread.native_id, self._calculate_time(time.time() - self.started))
+
+    def get_info(self) -> ProcessInfo:
+        return ProcessInfo(self.id,
+                           self.owner,
+                           self.name,
+                           self.native_id,
+                           self._calculate_time(time.time() - self.started),
+                           self.is_reserved_process
+                           )
+
+    def __str__(self) -> str:
+        return self.get_info().__str__()
 
     def _calculate_time(self, seconds: float) -> str:
         minutes = int(seconds / 60)
@@ -33,13 +72,14 @@ class Process:
 
         return f"{seconds}s"
 
-    def run(self, is_main_thread = False):
+    def run(self, is_main_thread=False):
         if is_main_thread:
-            self.thread = Thread(target=self._run_main, name=self.line_args[0])
+            self.thread = Thread(target=self._run_main, name=self.name)
             self.thread.start()
+            self.native_id = self.thread.native_id
             return
-        
-        self.thread = Thread(target=self._run, name=self.line_args[0], daemon=True)
+
+        self.thread = Thread(target=self._run, name=self.name, daemon=True)
         self.thread.start()
 
     def _run_main(self):
@@ -51,12 +91,6 @@ class Process:
         self.command_instance.close()
         self.status = self.command_instance.exit()
 
-    def __str__(self) -> str:
-        return f"{self.id}\t{self.owner}\t{self.thread.name}\t{self._calculate_time(time.time() - self.started)}"
-
-    def all_info(self) -> str:
-        return f"{self.id}\t{self.owner}\t{self.thread.name}\t{self.thread.native_id}\t{self._calculate_time(time.time() - self.started)}"
-
 
 class Processes:
     def __init__(self):
@@ -67,17 +101,19 @@ class Processes:
         # Avoid returning the system managed list of processes
         # Instead return a copy
         return self.processes.copy()
-    
+
     def _generate_pid(self) -> int:
         self.process_counter += 1
         return self.process_counter
 
     def _add_main_process(self, info: object, prog_name: str, callable: Callable):
-        self.processes.append(Process(id=self._generate_pid(),owner=info.user.username, command_instance=callable, line_args=prog_name))
+        self.processes.append(Process(id=self._generate_pid(), owner=info.user.username,
+                              command_instance=callable, line_args=prog_name, is_reserved_process=True))
         self.processes[-1].run(is_main_thread=True)
 
-    def add(self, info: object, line_args: List[str], callable: Callable):
-        self.processes.append(Process(id=self._generate_pid(),owner=info.user.username, command_instance=callable, line_args=line_args))
+    def add(self, info: object, line_args: List[str], callable: Callable, is_reserved: bool):
+        self.processes.append(Process(id=self._generate_pid(), owner=info.user.username,
+                              command_instance=callable, line_args=line_args, is_reserved_process=is_reserved))
         print(f"[{self.processes[-1].id}] {line_args[0]}")
         self.processes[-1].run()
         time.sleep(.1)
