df820553374595b7dd7318e6aca48f1fb29b60b4
RoysManfo <manforoys@gmail.com>
2023-09-11 20:44:13 +0200

    Fix commnets, add warning() and colored output

--- src/core/helpers/commands.py
+++ src/core/helpers/commands.py
@@ -2,7 +2,9 @@ from src.settings.info import Info
 import sys
 from typing import TextIO
 from src.core.system.processes import *
+from colorama import init as col_init, Fore
 
+col_init(autoreset=True)
 
 class CommandInterface:
     """
@@ -14,19 +16,26 @@ class CommandInterface:
     ### GENERAL ATTRIBUTES
     Attributes shared by commands
 
-    - `IS_PROCESS (const, bool)`     Whether or not the command is being runned as a thread 
+    - `IS_PROCESS (const, bool)`     Whether or not the command is being runned as a background thread 
     - `info (variable, Info)`       A reference to the instance of the Info class, containing process information 
     - `stdout (variable, TextIO)`   The stdout of the command
     - `stderr (variable, TextIO)`   The stderr of the command
     - `stdin (variable, TextIO)`    The stdin of the command
 
-    ### AUTOMATIC FUNCTIONS
-    Functions that get called regardless by the terminal
+    ### AUTOMATIC CALLS
+    Methods that get called regardless by the terminal
 
-    - `init()`      This is function is called right before run().
-    - `run()`       This is the entry function for the command.
-    - `close()`     This is the function that gets called right after run() the command. 
-    - `exit()`      This is the last function that gets called.
+    - `init()`      This method is called right before run().
+    - `run()`       This is the entry method for the command.
+    - `close()`     This is the method that gets called right after run() the command. 
+    - `exit()`      This is the last method that gets called.
+
+    ### HELPER FUNCTIONS
+    Other usefull methods, NOT called by the terminal.
+    If you want to use these methods you need to call them yourself.
+
+    - `error()`     This function should be called once an error accoures.
+    - `warning()`   This function should be called to issue warnings.
 
     """
 
@@ -83,10 +92,38 @@ class CommandInterface:
     HELPER FUNCTIONS
     """
 
-    def error(self, status: int | None = None, msg: str | None = None):
+    def error(self, status: int | None = None, msg: str | None = None, use_color: bool = False):
         """
-        This function should be called once an error accoures errors.\n
+        This function should be called once an error accoures.\n
         This function should be called to handle errors.
+
+        By default sets the status to STATUS_ERR.
         """
-        self.stderr.write(f"{self.parser.prog}: {msg}\n\n")
-        self.status = status
+        if use_color:
+            self.stderr.write(f"{Fore.RED}{self.parser.prog}: {msg}{Fore.RESET}\n\n")
+        else:
+            self.stderr.write(f"{self.parser.prog}: {msg}\n\n")
+        self.status = status or STATUS_ERR
+
+    def warning(self, status: int | None = None, msg: str | None = None, use_color: bool = False, to_stdout: bool = True):
+        """
+        This function should be called to issue warnings.\n
+        This function should be called to handle warnings (by default writes to stdout).
+
+        By default sets the status to STATUS_WARN.
+        """
+        
+        if to_stdout:
+            if use_color:
+                self.stdout.write(f"{Fore.YELLOW}{self.parser.prog}: {msg}{Fore.RESET}\n")
+            else:
+                self.stdout.write(f"{msg}\n")
+        else:
+            if use_color:
+                self.stderr.write(f"{Fore.YELLOW}{self.parser.prog}: {msg}{Fore.RESET}\n")
+            else:
+                self.stderr.write(f"{msg}\n")
+
+
+
+        self.status = status or STATUS_WARN
