51195965104d0054c289fe72198ef52225fa44d0
RoysManfo <manforoys@gmail.com>
2023-10-20 23:39:11 +0200

    Addedsome  preliminary checks

--- src/core/cmd/builtin/mv.py
+++ src/core/cmd/builtin/mv.py
@@ -1,6 +1,7 @@
-from ...helpers.commands import CommandInterface
+from ...helpers.commands import *
 from ...helpers.arguments import Parser
 
+import os 
 import shutil
 
 class Command(CommandInterface):
@@ -19,5 +20,55 @@ class Command(CommandInterface):
         """
         This command tells a random programming joke
         """
+
+        for i in self.args.source:
+            if not os.path.exists(i):
+                self.error(STATUS_ERR, self.logger.file_not_found(i))
+                return
+
+        # Handle multiple files given as source to copy
+        if len(self.args.source) > 0 or not os.path.exists(self.args.dest) or not os.path.isdir(self.args.dest):            
+            try:
+                os.makedirs(self.args.dest)
+
+            except PermissionError:
+                self.error(STATUS_ERR, self.logger.permission_denied(self.args.dest))
+                return
+
+
+        if self.args.interactive and self.args.no_clobber:
+            a = ''
+            i = ['-i', '--interactive']
+            n = ['-n', '--no-clobber']
+
+            for arg in self.command[::-1]:
+                if arg in i + n:
+                    a = arg
+                    break
+
+            if a in i:
+                self.args.no_clobber = False
+            else:
+                self.args.interactive = False
+
+        for path in self.args.source:
+            if os.path.isdir(path):
+                try:
+                    self.args.source.extend(os.listdir(path))
+
+                except PermissionError:
+                    self.error(STATUS_ERR, self.logger.permission_denied(path))
+                    return
+
+
+        dest = self.args.dest            
+        for path in self.args.source:            
+            # self.move_file(path)
+            pass
+            # reset the destination if it has been modified (es. in copy_file())
+            # self.args.dest = dest
+
+        print()
+
 
                 
