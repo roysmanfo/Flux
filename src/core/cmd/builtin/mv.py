89ac926c52c7a419466c4868ed2ad850da6ddba2
RoysManfo <manforoys@gmail.com>
2023-10-21 00:50:09 +0200

    Implemented logic

--- src/core/cmd/builtin/mv.py
+++ src/core/cmd/builtin/mv.py
@@ -11,7 +11,7 @@ class Command(CommandInterface):
 
         self.parser.add_argument('source', nargs='+', help='the file(s) or directory(s) to copy')
         self.parser.add_argument('dest', help='the destination of the sources')
-        self.parser.add_argument('-f', '--force', action='store_true', help='do not prompt before overwriting (overrides a previous -i or -n)')
+        self.parser.add_argument('-f', '--force', action='store_true', default=True, help='do not prompt before overwriting (overrides a previous -i or -n)')
         self.parser.add_argument('-i', '--interactive', action='store_true', help='prompt before overwrite (overrides a previous -n or -f)')
         self.parser.add_argument('-n', '--no-clobber', action='store_true', help='do not overwrite an existing file (overrides previus -i or -f)')
         self.parser.add_argument('-v', '--verbose', action='store_true', help='explain what is being done')
@@ -36,39 +36,83 @@ class Command(CommandInterface):
                 return
 
 
-        if self.args.interactive and self.args.no_clobber:
+        if self.args.force and self.args.interactive and self.args.no_clobber:
             a = ''
             i = ['-i', '--interactive']
             n = ['-n', '--no-clobber']
+            f = ['-f', '--force']
 
             for arg in self.command[::-1]:
-                if arg in i + n:
+                if arg in i + n + f:
                     a = arg
                     break
 
             if a in i:
                 self.args.no_clobber = False
-            else:
+                self.args.force = False
+            elif a in n:
                 self.args.interactive = False
+                self.args.force = False
+            else:
+                self.args.no_clobber = False
+                self.args.interactive = False
+
 
         for path in self.args.source:
             if os.path.isdir(path):
                 try:
                     self.args.source.extend(os.listdir(path))
+                    self.args.source.remove(path)
 
                 except PermissionError:
                     self.error(STATUS_ERR, self.logger.permission_denied(path))
                     return
 
+        self.args.source.sort()
 
-        dest = self.args.dest            
+        # dest = self.args.dest            
         for path in self.args.source:            
-            # self.move_file(path)
-            pass
+            self.move(path)
+
+
             # reset the destination if it has been modified (es. in copy_file())
             # self.args.dest = dest
 
         print()
 
 
+
+    def move(self, path: str):
+        d = self.args.dest if os.path.isdir(self.args.dest) else os.path.dirname(self.args.dest)
+        final_path = os.path.join(self.args.dest, path)
+
+        if os.path.exists(final_path):
+            if self.args.no_clobber:
+                return
+
+            if self.args.interactive:
+                overwrite = self.stdin.read().strip() if self.stdin.readable() else ''
+
+                while not overwrite.lower().strip() in ['y', 'n']:
+                    overwrite = input(f"cp: overwrite '{self.args.dest}'? ")
+                    if not overwrite.lower().strip() in ['y', 'n']:
+                        print("please specify 'y' or 'n'")
+
+                if overwrite == 'n':
+                    return
                 
+
+            if self.args.force:
+                try:
+                    os.remove(d)
+                except PermissionError:
+                    self.error(STATUS_ERR, self.logger.permission_denied(d))
+                    return
+
+        dest = shutil.move(path, d)
+
+        if self.args.verbose:
+            print('%s -> %s' % (path, dest))
+
+
+
