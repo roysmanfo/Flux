92db5976ec19ccf959e56ae06ab422f6adb29c9a
RoysManfo <manforoys@gmail.com>
2023-12-08 18:02:31 +0100

    Support for parameters

--- src/core/cmd/builtin/ext.py
+++ src/core/cmd/builtin/ext.py
@@ -9,13 +9,24 @@ class Command(CommandInterface):
     
     def init(self):
         self.parser = Parser("ext", description="Allows to use commands from outside flux")
-        self.parser.add_argument("command", nargs="*", help="the command to run")
+        self.parser.add_argument("command", action="append", help="the command to run")
+
+    def setup(self):
+        if "-h" in self.command or "--help" in self.command:
+            super().setup()
+
+        elif len(self.command[1:]) == 0 :
+            self.error(self.logger.parameter_not_specified("command"))
+            self.parser.exit_execution = True
+            return
+
+        self.command = self.command[1:]
 
     def run(self):
         global p
-        p = subprocess.Popen(self.args.command, stdin=self.stdin, stdout=self.stdout, stderr=self.stderr, text=True)
+        p = subprocess.Popen(self.command, stdin=self.stdin, stdout=self.stdout, stderr=self.stderr, text=True)
         p.wait()
     
     def close(self):
-        self.status = p.returncode
+        self.status = p.returncode if p else None
 
