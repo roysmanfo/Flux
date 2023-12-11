e6e8351659302dfe2a0ca2c004a446d039360ac1
RoysManfo <manforoys@gmail.com>
2023-12-11 23:25:37 +0100

    removed env variables and related tests

--- tests/test_env_variables.py
+++ tests/test_env_variables.py
@@ -1,19 +1,8 @@
 import unittest
-import os
-from dotenv import load_dotenv
 
 
 class Test_EnvVariables(unittest.TestCase):
-    def test_CMD_variables_type(self):
-
-        load_dotenv(verbose=False)
-
-        WINDOWS = os.getenv("WINDOWS").split(", ")
-        LINUX = os.getenv("LINUX").split(", ")
-
-        result = [type(WINDOWS) == type(LINUX)]
-
-        self.assertTrue(all(result))
+    pass
 
 
 if __name__ == '__main__':
