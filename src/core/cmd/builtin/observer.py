f1543dbe993603fa401ecbe4480c5cd28d8cdced
RoysManfo <manforoys@gmail.com>
2023-12-19 22:11:11 +0100

    Moved location of the extensions in `~/.flux/.local/observer/extensions`

--- src/core/cmd/builtin/observer.py
+++ src/core/cmd/builtin/observer.py
@@ -29,17 +29,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import json
 import os
 import shutil
 from pathlib import Path
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, DirModifiedEvent
-from ...helpers.extensions import extension_paths
 from ...helpers.arguments import Parser
 from ...helpers.commands import CommandInterface
 
-
+extension_paths = {}
 class Command(CommandInterface):
 
     def init(self) -> None:
@@ -55,6 +55,26 @@ options:
                 - Bucket: Where this command will look for new files
                 - Destination: Where the files will be sorted
 """)
+
+    def setup(self):
+        global extension_paths
+        super().setup()
+
+        self.jokes: list[str] = []
+        jpath = os.path.join(self.info.syspaths.LOCAL_FOLDER, "observer", "extensions.json")
+        try:
+            with open(jpath) as f:
+                extension_paths = json.load(f)
+        
+        except FileNotFoundError:
+            os.makedirs(os.path.dirname(jpath), exist_ok=True) # exists_ok=True in case just the file is missing
+            with open(jpath, "w") as f:
+                extension_paths = EXTENSIONS
+                json.dump(extension_paths, f, indent=4)
+            
+        except PermissionError:
+            self.error(self.logger.permission_denied(jpath))
+            self.parser.exit_execution = True
 
     def run(self) -> None:
 
@@ -201,3 +221,152 @@ options:
                     destination_path = self.rename_file(
                         child, destination_path)
                     shutil.move(src=child, dst=destination_path)
+
+# TODO: Not store extensions here but add an alternative method to restore externsions.json 
+EXTENSIONS = {
+    # No name
+    'noname':  'other/uncategorized',
+    # audio
+    '.aif':    'media/audio',
+    '.cda':    'media/audio',
+    '.mid':    'media/audio',
+    '.midi':   'media/audio',
+    '.mp3':    'media/audio',
+    '.mpa':    'media/audio',
+    '.ogg':    'media/audio',
+    '.wav':    'media/audio',
+    '.wma':    'media/audio',
+    '.wpl':    'media/audio',
+    '.m3u':    'media/audio',
+    # text
+    '.txt':    'text/text_files',
+    '.doc':    'text/microsoft/word',
+    '.docx':   'text/microsoft/word',
+    '.odt ':   'text/text_files',
+    '.pdf':    'text/pdf',
+    '.rtf':    'text/text_files',
+    '.tex':    'text/text_files',
+    '.wks ':   'text/text_files',
+    '.wps':    'text/text_files',
+    '.wpd':    'text/text_files',
+    # video
+    '.3g2':    'media/video',
+    '.3gp':    'media/video',
+    '.avi':    'media/video',
+    '.flv':    'media/video',
+    '.h264':   'media/video',
+    '.m4v':    'media/video',
+    '.mkv':    'media/video',
+    '.mov':    'media/video',
+    '.mp4':    'media/video',
+    '.mpg':    'media/video',
+    '.mpeg':   'media/video',
+    '.rm':     'media/video',
+    '.swf':    'media/video',
+    '.vob':    'media/video',
+    '.wmv':    'media/video',
+    # images
+    '.ai':     'media/images',
+    '.bmp':    'media/images',
+    '.gif':    'media/images',
+    '.jpg':    'media/images',
+    '.jpeg':   'media/images',
+    '.png':    'media/images',
+    '.ps':     'media/images',
+    '.psd':    'media/images',
+    '.svg':    'media/images',
+    '.tif':    'media/images',
+    '.tiff':   'media/images',
+    '.cr2':    'media/images',
+    # internet
+    '.asp':    'other/internet',
+    '.aspx':   'other/internet',
+    '.cer':    'other/internet',
+    '.cfm':    'other/internet',
+    '.cgi':    'other/internet',
+    '.pl':     'other/internet',
+    '.css':    'other/internet',
+    '.htm':    'other/internet',
+    '.js':     'other/internet',
+    '.jsp':    'other/internet',
+    '.part':   'other/internet',
+    '.php':    'other/internet',
+    '.rss':    'other/internet',
+    '.xhtml':  'other/internet',
+    '.html':   'other/internet',
+    # compressed
+    '.7z':     'other/compressed',
+    '.arj':    'other/compressed',
+    '.deb':    'other/compressed',
+    '.pkg':    'other/compressed',
+    '.rar':    'other/compressed',
+    '.rpm':    'other/compressed',
+    '.tar.gz': 'other/compressed',
+    '.z':      'other/compressed',
+    '.zip':    'other/compressed',
+    # disc
+    '.bin':    'other/disc',
+    '.dmg':    'other/disc',
+    '.iso':    'other/disc',
+    '.toast':  'other/disc',
+    '.vcd':    'other/disc',
+    # data
+    '.csv':    'programming/database',
+    '.dat':    'programming/database',
+    '.db':     'programming/database',
+    '.dbf':    'programming/database',
+    '.log':    'programming/database',
+    '.mdb':    'programming/database',
+    '.sav':    'programming/database',
+    '.sql':    'programming/database',
+    '.tar':    'programming/database',
+    '.xml':    'programming/database',
+    '.json':   'programming/database',
+    # executables
+    '.apk':    'other/executables',
+    '.bat':    'other/executables',
+    '.com':    'other/executables',
+    '.exe':    'other/executables',
+    '.gadget': 'other/executables',
+    '.jar':    'other/executables',
+    '.wsf':    'other/executables',
+    # fonts
+    '.fnt':    'other/fonts',
+    '.fon':    'other/fonts',
+    '.otf':    'other/fonts',
+    '.ttf':    'other/fonts',
+    # presentations
+    '.key':    'text/presentations',
+    '.odp':    'text/presentations',
+    '.pps':    'text/presentations',
+    '.ppt':    'text/presentations',
+    '.pptx':   'text/presentations',
+    # programming
+    '.c':      'programming/c&c++',
+    '.class':  'programming/java',
+    '.java':   'programming/java',
+    '.py':     'programming/python',
+    '.sh':     'programming/shell',
+    '.h':      'programming/c&c++',
+    # spreadsheets
+    '.ods':    'text/microsoft/excel',
+    '.xlr':    'text/microsoft/excel',
+    '.xls':    'text/microsoft/excel',
+    '.xlsx':   'text/microsoft/excel',
+    # system
+    '.bak':    'text/other/system',
+    '.cab':    'text/other/system',
+    '.cfg':    'text/other/system',
+    '.cpl':    'text/other/system',
+    '.cur':    'text/other/system',
+    '.dll':    'text/other/system',
+    '.dmp':    'text/other/system',
+    '.drv':    'text/other/system',
+    '.icns':   'text/other/system',
+    '.ico':    'text/other/system',
+    '.ini':    'text/other/system',
+    '.lnk':    'text/other/system',
+    '.msi':    'text/other/system',
+    '.sys':    'text/other/system',
+    '.tmp':    'text/other/system'
+}
