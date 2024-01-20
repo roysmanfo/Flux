daa2e755aaddbaad2be750394e0769a3e7fb366f
RoysManfo <manforoys@gmail.com>
2024-01-20 20:12:04 +0100

    Creating System class that will contain all information non accessible without enough privileges

---
+++ src/core/system/system.py
@@ -0,0 +1,12 @@
+from src.settings.info import Info, VERSION
+
+
+class System:
+    def __init__(self, sysinfo: Info) -> None:
+        self.exit = False
+        self.sysinfo = sysinfo
+        self.version = VERSION
+
+    def get_info(self) -> Info:
+        return self.sysinfo.copy()
+
