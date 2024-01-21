d625b79d99674c66a34f748f2c19936db1f64e81
RoysManfo <manforoys@gmail.com>
2024-01-22 00:01:36 +0100

    Modified setup structure

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,20 +2,32 @@ import unittest
 
 
 class Test_TestUser(unittest.TestCase):
-
+    
     def test_user_has_correct_attributes(self):
         """
         Check if all attributes heve the desired type
         """
 
-        from src.settings.info import User, Path, BgTasks
-        USER = User()
+        import os
+        from src.settings.info import Path, BgTasks, SysPaths
+        from src.core import setup
+
+        s_file_exists = os.path.exists(SysPaths.SETTINGS_FILE)
+
+        info = setup.setup()
+        USER = info.user
         types = [
             type(USER.email) == str,
             type(USER.paths) == Path,
             type(USER.background_tasks) == list,
         ]
         result = all(types)
+
+        if not s_file_exists:
+            # check if it has been created before deleting
+            if os.path.exists( SysPaths.SETTINGS_FILE):
+                os.remove(SysPaths.SETTINGS_FILE)
+
         self.assertTrue(
             result, "1 or more attributes are identified with the wrong type")
 
