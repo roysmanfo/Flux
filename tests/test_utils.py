7be75dea0175405fbda183b2d21e7f87a438d3c6
RoysManfo <manforoys@gmail.com>
2024-01-24 00:31:55 +0100

    Added some tests for corect command line parsing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,6 +22,40 @@ class Test_TestUtils(unittest.TestCase):
         formatted = transform.string_to_list(test)
         self.assertTrue(formatted == ["hello", "World!"])
 
+class Test_TestStringToList(unittest.TestCase):
+
+    def test_01(self):
+        cmd = transform.string_to_list("ls")
+        self.assertTrue(cmd == ["ls"], cmd)
+
+    def test_02(self):
+        cmd = transform.string_to_list("   ls   ")
+        self.assertTrue(cmd == ["ls"], cmd)
+
+    def test_03(self):
+        cmd = transform.string_to_list("LS -h")
+        self.assertTrue(cmd == ["ls", "-h"], cmd)
+
+    def test_04(self):
+        cmd = transform.string_to_list("LS -R")
+        self.assertTrue(cmd == ["ls", "-R"], cmd)
+
+    def test_05(self):
+        cmd = transform.string_to_list("ls >> file")
+        self.assertTrue(cmd == ["ls", ">>", "file"], cmd)
+
+    def test_06(self):
+        cmd = transform.string_to_list("ls 2>/dev/null")
+        self.assertTrue(cmd == ["ls", "2>", "/dev/null"], cmd)
+
+    def test_07(self):
+        cmd = transform.string_to_list("LS << Input.txt")
+        self.assertTrue(cmd == ["ls", "<<", "Input.txt"], cmd)
+
+    def test_08(self):
+        cmd = transform.string_to_list("echo test | ls")
+        self.assertTrue(cmd == ["echo", "test", "|", "ls"], cmd)
+
 
 if __name__ == '__main__':
     unittest.main()
