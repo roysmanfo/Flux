ead9193401c2d6c54d25e445e0d0aa1fc74bea7f
RoysManfo <manforoys@gmail.com>
2024-02-10 00:11:38 +0100

    New method to get the pip command

--- flux/utils/environment.py
+++ flux/utils/environment.py
@@ -33,3 +33,23 @@ def get_interpreter_command() -> Optional[str]:
 
     return None
 
+def get_pip_command() -> Optional[str]:
+    """
+    `:returns` : the command to use in order to interact with the python pip package manager, None if not found
+    `:dtype`   : str or None
+    """
+
+    commands = [
+        ("pip3", "--version"),
+        ("pip", "--version"),        
+    ]
+
+    for c in commands:
+        try:
+            p = subprocess.run(c, capture_output=True)
+            if p and p.stdout:
+                return c[0]
+        except:
+            pass
+
+    return None
