e3b223ba0d3bd485d339f990b6afe1ae21194d20
RoysManfo <manforoys@gmail.com>
2024-02-10 22:29:07 +0100

    Removed unnecessary method

--- flux/core/setup.py
+++ flux/core/setup.py
@@ -51,27 +51,3 @@ def setup() -> Info:
     return INFO
 
 
-def get_interpreter_command() -> Optional[str]:
-    """
-    `:returns` : the command to use in order to interact with the python interpreter cli, None if not found
-    `:rtype`   : str | None
-    """
-
-    commands = [
-        ("python3", "--version"),
-        ("python", "--version"),
-        
-        # found on Windows
-        ("py", "--version"),
-    ]
-
-    for c in commands:
-        try:
-            p = subprocess.run(c, capture_output=True)
-            if p and p.stdout:
-                return c[0]
-        except:
-            pass
-
-    return None
-
