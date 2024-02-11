684a9b9e121fd81b53942238c14e25695fae3a07
RoysManfo <manforoys@gmail.com>
2024-02-11 20:54:21 +0100

    Removed some comments

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,19 +5,11 @@ from flux.utils import transform
 class Test_TestUtils(unittest.TestCase):
 
     def test_returns_a_list(self):
-        """
-        Check if the command.py util works properly
-        """
-
         test = "   Hello   World!   "
         formatted = transform.string_to_list(test)
         self.assertFalse(formatted == ["Hello", "World!"])
 
     def test_returns_a_list_2(self):
-        """
-        Check if the command.py util works properly
-        """
-
         test = "   Hello   World!   "
         formatted = transform.string_to_list(test)
         self.assertTrue(formatted == ["hello", "World!"])
