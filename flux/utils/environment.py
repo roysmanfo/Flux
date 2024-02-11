d2d23a72dd3060c192079ad06d58efd58c3935de
RoysManfo <manforoys@gmail.com>
2024-02-11 20:31:07 +0100

    Added some methods

--- flux/utils/environment.py
+++ flux/utils/environment.py
@@ -4,6 +4,18 @@ from typing import Optional, Tuple
 import os
 
 
+def get_venv_location() -> Optional[str]:
+    """
+    `:returns` : returns the virtual environment location
+    `:rtype`   : str | None
+    """
+    if os.getenv("VIRTUAL_ENV"):
+        return os.getenv("VIRTUAL_ENV")
+    elif os.getenv("CONDA_PREFIX"):
+        return os.getenv("CONDA_PREFIX")
+    return None
+
+
 def get_venv() -> Optional[Tuple[str, str]]:
     """
     `:returns` : a tuple containing the virtual environment name and type (VENV | CONDA)
@@ -15,12 +27,34 @@ def get_venv() -> Optional[Tuple[str, str]]:
         return os.path.basename(os.getenv("CONDA_PREFIX")), "CONDA"
     return None
 
+
 def get_venv_name() -> Optional[str]:
     venv = get_venv()
     return None if not venv else venv[0]
 
+
 def is_in_venv() -> bool:
+    """
+    `:returns` : True if the application is running in a virtual environment  
+    `:rtype`   : bool
+    """
     return get_venv() is not None
+
+
+def is_in_flux_env() -> bool:
+    """
+    `:returns` : True if the application is running in a Flux virtual environment  
+    `:rtype`   : bool
+    """
+    venv_l = get_venv_location()
+
+    list
+    return all([
+            is_in_venv(),
+            ".fluxenv" in os.listdir(venv_l),
+            venv_l == os.path.expanduser(".flux/venv")
+        ])
+
 
 def get_interpreter_command() -> str:
     """
@@ -29,6 +63,7 @@ def get_interpreter_command() -> str:
     """
 
     return os.path.splitext(os.path.basename(sys.executable))[0]
+
 
 def get_pip_command() -> str:
     """
