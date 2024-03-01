fe80d28650716155a30e34712a7e2715ce991783
RoysManfo <manforoys@gmail.com>
2024-03-01 23:48:37 +0100

    Separate commands when a semicolon is found

--- flux/utils/transform.py
+++ flux/utils/transform.py
@@ -1,5 +1,5 @@
 import shlex
-from typing import List
+from typing import List, Tuple, Union
 import sys as _sys
 
 def string_to_list(string: str) -> List[str]:
@@ -35,6 +35,7 @@ def _separate_redirect_parts(arg: str) -> List[str]:
     ```py
     separate_redirect_parts("2>/dev/null") -> ["2>", "/dev/null"]
     separate_redirect_parts("/dev/null") -> ["/dev/null"]
+    separate_redirect_parts("ls;pwd") -> ["ls;pwd"]
     """
 
     # This order is NOT random
@@ -42,16 +43,69 @@ def _separate_redirect_parts(arg: str) -> List[str]:
     LONG_REDIRECT = ["1>>", "2>>", "&>>", "<<<"]
     MEDIUM_REDIRECT = [">>", "<<", "1>", "2>", "&>", "|&"]
     SHORT_REDIRECT = [">", "<", "|"]
+    COMMAND_SEPARATOR = [';']
 
-    LOOK_FOR = LONG_REDIRECT + MEDIUM_REDIRECT + SHORT_REDIRECT
+    LOOK_FOR = COMMAND_SEPARATOR + LONG_REDIRECT + MEDIUM_REDIRECT + SHORT_REDIRECT
 
     for i in LOOK_FOR:
-        if arg.startswith(i):
-            if arg == i:
-                return [i]
-            
+        if arg == i:
+            return [i]
+        
+        if i in COMMAND_SEPARATOR and i in arg:  
+            z = arg.split(i)
+            res = []
+            for j in z:
+                res.append(j)
+                if j != z[-1]:# or (len(arg) > 0 and arg[-1] == i):
+                    res.append(i)
+
+            return res
+        elif arg.startswith(i):
             # python 3.8 does not have removeprefix
             if _sys.version_info >= (3, 9):
                 return [i, arg.removeprefix(i)]
             return [i, arg[len(i):]]
-    return [arg]
+        
+        elif arg.endswith(i):
+            # python 3.8 does not have removeprefix
+            if _sys.version_info >= (3, 9):
+                return [arg.removesuffix(i), i]
+            return [-arg[len(i):], i]
+        
+        
+    return [arg] 
+
+
+
+def split_commands(args: Union[str, List[str]]) -> List[List[str]]:
+    """
+    This takes as input a string or list of strings and separates the
+    diferent commands when a separator is found semicolon is found
+
+    ```py
+    split_commands('echo test; ls') -> [["echo", "test"], ["ls"]]
+    split_commands(["echo", "test", ";", "ls"]) -> [["echo", "test"], ["ls"]]
+    ```
+
+    Note: this currently works better with a string as input
+    """
+
+    if isinstance(args, str):
+        args = string_to_list(args)
+
+    res = []
+    push = []
+    for i in args:
+        if i == ";":
+            if push:
+                res.append(push)
+                push = []
+        else:
+            if i:
+                push.append(i)
+
+    if push:
+        res.append(push)
+
+    return res
+
