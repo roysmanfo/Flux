52c99819fe68e89214a73e03f87e58846ba25e8c
RoysManfo <manforoys@gmail.com>
2024-05-03 21:50:22 +0200

    Connection as server supported

--- flux/core/cmd/builtin/nc.py
+++ flux/core/cmd/builtin/nc.py
@@ -8,10 +8,11 @@ ENTRY_POINT = "Netcat"
 
 class Netcat(CommandInterface):
     def init(self):
-        self.parser = Parser("netcat", description="arbitrary TCP and UDP connections and listens")
+        self.parser = Parser("nc", description="arbitrary TCP and UDP connections and listens")
         self.parser.add_argument("destination", nargs="?")
         self.parser.add_argument("port", nargs="?")
         self.parser.add_argument("-l", action="store_true", help="Listen mode, for inbound connects")
+        self.parser.add_argument("-p", dest="PORT", help="Specify local port for remote connects")
 
     def setup(self):
         super().setup()
@@ -20,28 +21,70 @@ class Netcat(CommandInterface):
             return
         
         if self.args.l:
-            self.args.port = self.args.destination
+            if not self.args.PORT:
+                self.error("missing port number")
+                return
 
+            self.args.port = self.args.PORT
+            
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             self.args.port = int(self.args.port)
-            sock.connect((self.args.destination, self.args.port))
             event = Event()
-            Thread(target=self.recv_messages, args=(sock, event), daemon=True).start()
 
-            try:
-                while not event.is_set():
-                    msg = self.input()
-                    if msg:
-                        sock.send(msg.encode())
-                    else:
-                        event.set()
 
-            except (KeyboardInterrupt, ConnectionAbortedError):
-                pass
-            finally:
-                event.set()
+            if self.args.l:
+                self.host_as_server(sock)
+            else:
+                sock.connect((self.args.destination, self.args.port))
+                Thread(target=self.recv_messages, args=(sock, event), daemon=True).start()
+
+                try:
+                    while not event.is_set():
+                        msg = self.input()
+                        if msg:
+                            sock.send(msg.encode())
+                        else:
+                            event.set()
+
+                except (KeyboardInterrupt, ConnectionAbortedError):
+                    pass
+                finally:
+                    event.set()
+
+    def host_as_server(self, sock: socket.socket) -> None:
+        event = Event()
+        sock.bind(("0.0.0.0", self.args.port))
+        sock.listen(5)
+
+        self.print(f"Listening on 0.0.0.0 {self.args.port}")
+        client, addr = sock.accept()
+        self.print(f"Connection received on {addr[0]} {addr[1]}")
+        Thread(target=self.recv_messages, args=(client, event), daemon=True).start()
+        self.send_messages(client, event)
+
+    def host_as_client(self, sock: socket.socket) -> None:
+        event = Event()
+
+        sock.connect((self.args.destination, self.args.port))
+        Thread(target=self.recv_messages, args=(sock, event), daemon=True).start()
+        self.send_messages(sock, event)
+
+    def send_messages(self, sock: socket.socket, event: Event) -> None:
+        try:
+            while not event.is_set():
+                msg = self.input()
+                if msg:
+                    sock.send(msg.encode())
+                else:
+                    event.set()
+
+        except (KeyboardInterrupt, ConnectionAbortedError):
+            pass
+        finally:
+            event.set()
+
 
     def recv_messages(self, sock: socket.socket, event: Event) -> None:
         try:
