4b54b695dc193c1d4bc9ba2458663f6cc6f99f25
RoysManfo <manforoys@gmail.com>
2024-05-04 22:51:40 +0200

    Reduce checks

--- flux/core/cmd/builtin/ext.py
+++ flux/core/cmd/builtin/ext.py
@@ -4,27 +4,30 @@ from flux.core.helpers.commands import *
 from flux.core.helpers.arguments import Parser
 
 
-
 p: subprocess.Popen = None
+
+
 class Command(CommandInterface):
-    
+
     def init(self):
-        self.parser = Parser("ext", description="Allows to use commands from outside flux")
-        self.parser.add_argument("command", action="append", help="the command to run")
+        self.parser = Parser(
+            "ext", description="Allows to use commands from outside flux")
+        self.parser.add_argument(
+            "command", action="append", help="the command to run")
 
     def setup(self):
         if "-h" in self.command and self.command.index("-h") == 1:
             super().setup()
-        
+
         elif "--help" in self.command and self.command.index("--help") == 1:
             super().setup()
-        
+
         elif len(self.command) < 2:
             # print an help message
             self.command.append("-h")
             super().setup()
 
-        elif len(self.command[1:]) == 0 :
+        elif len(self.command[1:]) == 0:
             self.error(self.errors.parameter_not_specified("command"))
             self.parser.exit_execution = True
             return
@@ -34,16 +37,10 @@ class Command(CommandInterface):
     def run(self):
         global p
 
-
         try:
             # find the executable
-            if os.name == 'nt':
-                process = subprocess.Popen(['where', self.command[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            else:
-                process = subprocess.Popen(['which', self.command[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
+            process = subprocess.Popen(['where' if os.name == 'nt' else "which",self.command[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, _ = process.communicate()
-
 
             if process.returncode == 0:
                 command_paths = [i.strip() for i in stdout.decode().strip().split('\n') if i]
@@ -61,9 +58,8 @@ class Command(CommandInterface):
                 if not success:
                     raise FileNotFoundError()
 
-
             else:
-               raise FileNotFoundError()
+                raise FileNotFoundError()
 
         except FileNotFoundError:
             self.error("unable to spawn the specified process (not found)")
@@ -72,4 +68,3 @@ class Command(CommandInterface):
     def close(self):
         if not self.status:
             self.status = p.returncode if p else STATUS_ERR
-
