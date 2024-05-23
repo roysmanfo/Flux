b0a7d83ef9610780a19c9500c359a08e70e9a80d
RoysManfo <manforoys@gmail.com>
2024-05-23 18:52:30 +0200

    list available signals

--- flux/core/cmd/builtin/kill.py
+++ flux/core/cmd/builtin/kill.py
@@ -10,7 +10,7 @@ class Command(CommandInterface):
                       "the signal named by SIGSPEC or SIGNUM. " \
                       "If neither SIGSPEC nor SIGNUM is present, then SIGTERM is assumed."
         self.parser = Parser("kill", description=description)
-        self.parser.add_argument("pid", help="the pid of the process")
+        self.parser.add_argument("pid", nargs="?", help="the pid of the process")
         self.parser.add_argument("-s", dest="sigspec", help="sigspec is a signal name")
         self.parser.add_argument("-n", dest="signum", type=int,  help="signum is a signal number")
         self.parser.add_argument("-l", dest="sigspec", nargs="?", type=int, help="list the signal names;if arguments follow `-l' they are assumed to be signal numbers for which names should be listed")
@@ -20,28 +20,47 @@ class Command(CommandInterface):
         self.listed = False
 
     def setup(self):
+        super().setup()
+        
         if "-l" in self.command or "-L" in self.command:
+            self.listed = True
             try:
                 signum = self.command[self.command.index("-l") + 1 if "-l" in self.command else self.command.index("-L") +1]
                 signum = int(signum)
 
                 name = interrupts.event_value_to_name(signum)
                 if name:
-                    print(name.lstrip("SIG"))
+                    self.print(name.lstrip("SIG"))
                     return
                 else:
                     raise IndexError()
                 
             except IndexError:
                 table = format.create_adaptive_table([f"{v}) {k}" for k, v in self.system.interrupt_handler.supported.items()])
-                print(table)
+                self.print(table)
             
             return
 
-        super().setup()
 
     def run(self):
-
         if self.listed:
             return
+
+        signum = interrupts.event_name_to_value("SIGTERM")
+
+        if self.args.sigspec:
+            signum = interrupts.event_name_to_value(self.args.sigspec)
+        elif self.args.signum:
+            signum = self.args.signum
+
+        if not signum:
+            self.error(f"{signum}: invalid signal specification")
+            return
+
+        if self.system.processes.find(self.args.pid):
+            pass
+        
+        proc = self.system.processes.remove(self.args.pid)
+        print(proc)
+
 
