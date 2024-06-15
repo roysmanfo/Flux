08370abd05290ecc6506cb632ad606e3e40a85fa
RoysManfo <manforoys@gmail.com>
2024-06-15 23:51:25 +0200

    Get request

--- flux/core/cmd/builtin/curl.py
+++ flux/core/cmd/builtin/curl.py
@@ -1,14 +1,21 @@
 from flux.core.helpers.commands import *
-import urllib.request
-import requests
 
+import requests
 
 
 class Command(CommandInterface):
     def init(self):
         self.parser = Parser("curl", description=" transfer a URL", usage="curl [options...] <url>")
-        self.parser.add_argument("url", nargs="?")
-    
-    def run(self):
-        print(self.args.url)
+        self.parser.add_argument("url", help="the page to visit")
+        self.parser.add_argument("-A", "--user-agent", dest="agent", default="curl", help="Send User-Agent <name> to server")
 
+        self.log_level = self.levels.INFO
+
+    def run(self):
+        res = requests.get(
+            url=self.args.url,
+            headers={"User-Agent": self.args.agent},
+        )
+
+        self.print(res.status_code, res.reason)
+        self.print(res.content)
