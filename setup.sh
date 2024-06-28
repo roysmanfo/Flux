8c7c805c8e349abbc6c9bda3cb3a1067fbdcd83b
RoysManfo <manforoys@gmail.com>
2024-06-28 23:18:41 +0200

    Added error handling functionalities

--- setup.sh
+++ setup.sh
@@ -8,31 +8,50 @@
 # 2. activate venv
 # 3. install requirements
 
-
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+DEVNULL="/dev/null"
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> $DEVNULL && pwd)
 SCRIPT_NAME=$(echo $0 | tr "/" "\n" | tail -n 1)
 SCRIPT_PATH=$(echo $SCRIPT_DIR/$SCRIPT_NAME)
 
 MAIN="$SCRIPT_DIR/flux/main.py"
-VENV="$SCRIPT_DIR/venv"
+VENV="$SCRIPT_DIR/.venv"
 REQIREMENTS="$SCRIPT_DIR/linux-requirements.txt"
 
-if [[ ! -d $VENV ]]; then
-    
+
+createVenv() {
     echo creating virtual environment in \'$VENV\'
-    mkdir $VENV -p 2>/dev/null
+    mkdir $VENV -p 2>$DEVNULL
     python3 -m venv --prompt flux $VENV
     echo -e "# This file has been automatically created by flux\n" > "$VENV/.fluxenv"
     source $(echo $VENV/bin/activate)
     echo installing requirements
     python3 -m pip install -r $REQIREMENTS
 
+    exit $?
+}
+
+
+if [[ ! -d $VENV ]]; then
+    createVenv
 else
-    source $VENV/bin/activate
+    source $VENV/bin/activate 2> $DEVNULL
 
     if [[ $? != 0 ]]; then
-        echo error in activation of \'$VENV\'
-        exit 1
+        echo Err: error in activation of \'$VENV\'
+        echo -n "recreate environment (y/n)?: "
+        read res
+        
+        if [[ $res != "y" ]]; then
+            echo $res
+            exit 1
+        fi 
+
+        # 'rm -rf' is often seen as a danger
+        # but in this case the path to delete
+        # is well defined and symlinks are not followed
+        echo removing \'$VENV\'
+        rm -rf $VENV
+        createVenv
     fi
 fi
 
