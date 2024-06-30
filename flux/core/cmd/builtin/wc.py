cab481126ed705fe6be563e09c5cb308ca11779b
RoysManfo <manforoys@gmail.com>
2024-06-30 22:52:13 +0200

    fix: Additional checks

--- flux/core/cmd/builtin/wc.py
+++ flux/core/cmd/builtin/wc.py
@@ -1,3 +1,4 @@
+import os
 from flux.core.helpers.commands import *
 
 class Command(CommandInterface):
@@ -10,10 +11,17 @@ class Command(CommandInterface):
         self.parser.add_argument("-w", "--words", action="store_true", help="print the newline counts")
 
     def run(self):
+
+        if self.args.file and not os.path.exists(self.args.file):
+            self.error(self.errors.file_not_found(self.args.file))
+            return
+        elif self.args.file and not os.path.isfile(self.args.file):
+            self.error(self.errors.cannot_read_dir(self.args.file))
+            return
+
         file = open(self.args.file, 'r') if self.args.file else self.stdin
         contents = file.read().strip().strip("\n")
         output = ""
-
         if self.parser.no_args:
             self.args.lines = True
             self.args.words = True
@@ -28,6 +36,8 @@ class Command(CommandInterface):
         if self.args.bytes:
             output += f"    {len(contents.encode("utf-8"))}"
 
+        if self.args.file:
+            output += " " + self.args.file
         self.print(output)
 
 
