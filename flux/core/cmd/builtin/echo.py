2ab52ca4173050db8a65445a468158eadb12a973
RoysManfo <manforoys@gmail.com>
2024-07-02 22:30:49 +0200

    Make the -E option able to overwrite -e

--- flux/core/cmd/builtin/echo.py
+++ flux/core/cmd/builtin/echo.py
@@ -10,14 +10,13 @@ class Command(CommandInterface):
         self.parser.add_argument("string", nargs="*", help="the text to display")
         self.parser.add_argument("-n", action="store_true", help="do not output the trailing newline")
         self.parser.add_argument("-e", action="store_true", help="enable interpretation of backslash escapes")
+        self.parser.add_argument("-E", action="store_true", help="disable interpretation of backslash escapes (default)")
         
-        self.parser.add_argument("-E", action="store_true", default=True, help="disable interpretation of backslash escapes (default)")
-        # ^^^ not actually needed ^^^
 
     def run(self):
         string = " ".join(self.args.string)
 
-        if self.args.e:
+        if self.args.e and not self.args.E:
             string = string.encode('utf-8').decode('unicode_escape')
 
         self.print(string, end="\n" if not self.args.n else "")
