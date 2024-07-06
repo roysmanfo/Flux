4031bceaf344ed36f7bbbbe8abb7d5938617a65c
RoysManfo <manforoys@gmail.com>
2024-07-06 23:38:59 +0200

    add: new features to create more dinamic tables

--- flux/utils/format.py
+++ flux/utils/format.py
@@ -1,9 +1,18 @@
 import os
-from typing import Iterable, Any, List, Union
+from typing import Iterable, Any, List, Optional, Union
 import re
 
 
-def create_table(*collumns: Union[str, List[str]], contents: Iterable[Iterable[Any]], show_headers: bool = True) -> str:
+def create_table(
+        *collumns: Union[str, List[str]],
+        contents: Iterable[Iterable[Any]],
+        show_headers: bool = True,
+        fill_value: Optional[Any] = "",
+        separator: str = "-",
+        add_top_line: bool = True,
+        add_bottom_line: bool = False,
+        last_is_footer: bool = False,
+    ) -> str:
     """
     Return a N x M table where N rappresents the number of columns
     and M rappresents the number of records in contents (+2 rows: 1 for the title and 1 for the underline)
@@ -25,25 +34,43 @@ def create_table(*collumns: Union[str, List[str]], contents: Iterable[Iterable[A
 
     """
 
+
     if collumns and isinstance(collumns[0], (list, tuple, set)):
         collumns = collumns[0]
+
     records: List[List[str]] = contents
+    fill_value = str(fill_value or "")
 
     n_columns = len(collumns)
-
-    column_widths = [max(len(str(record[i])) for record in records) + 2 for i in range(n_columns)]
-    column_widths = [max(len(collumn), width) + 2 for collumn, width in zip(collumns, column_widths)]
+    
+    column_widths = [
+        max(
+            len(str(record[i]) if i < len(record) else fill_value)
+            for record in records
+        ) + 2 for i in range(n_columns)
+    ]
+    column_widths = [max(len(collumn), width) + 2 for collumn,
+                     width in zip(collumns, column_widths)]
 
     output = ""
+    # add the headers
     if show_headers:
         output += "   ".join(f"{collumn}{' ' * (width - len(collumn))}" for collumn, width in zip(collumns, column_widths)) + "\n"
-        output += "     ".join("-" * (width - 2) for width in column_widths) + "\n"
+        output += "     ".join((separator if add_top_line else ' ') * (width - 2) for width in column_widths) + "\n"
+    
+    # add a footer separator
+    if last_is_footer:
+        records.insert(-1, tuple((separator if add_top_line else ' ') * (width - 2) for width in column_widths))
 
+    # add all rows (ignores a row's column if there is no header for it)
     for record in records:
-        output += "   ".join(f"{str(record[i])}{' ' * (width - len(str(record[i])))}" for i, width in enumerate(column_widths)) + "\n"
-
-    output += "\n"
-
+        output += "   ".join(f"{str(record[i]) if i < len(record) else fill_value}{' ' * (width - len(str(
+            record[i]) if i < len(record) else fill_value))}" for i, width in enumerate(column_widths)) + "\n"
+    
+    # manage footer
+    if add_bottom_line:
+        output += "     ".join(separator * (width - 2) for width in column_widths) + "\n"
+        
     return output
 
 def create_adaptive_table(data: Iterable[str]) -> str:
