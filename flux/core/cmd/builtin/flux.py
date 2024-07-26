2e838980bb6d739a0d9819559e50ea42bc3f4d2f
RoysManfo <manforoys@gmail.com>
2024-07-26 23:34:53 +0200

    add: refactor `flux` command

--- flux/core/cmd/builtin/flux.py
+++ flux/core/cmd/builtin/flux.py
@@ -4,31 +4,55 @@ from flux.core.helpers.commands import (
     Parser
 )
 
+EMOTES = [
+    r"(ง ͠° ͟ل͜ ͡°)ง",
+    r"(╯°□°)╯︵ ┻━┻",
+    r"(ノಠ益ಠ)ノ彡┻━┻",
+    r"┬─┬ノ( º _ ºノ)",
+    r"¯\_(ツ)_/¯",
+    r"( ͡◉ ͜ʖ ͡◉)",
+    r"(  ಠ_ಠ )",
+    r"(ꐦ ಠ皿ಠ )",
+    r" ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з=(◣_◢)=ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ ",
+    r"ᕦ(⌐■ ͜ʖ■)ᕥ",
+    r"\_(ಠ_ಠ)_/",
+    r"┻━┻ ¯\_(ಠ□ಠ)_/¯ ┻━┻",
+    r"(ノಠ益ಠ)ノ彡┻━┻ ლ(ಠ益ಠლ)",
+]
+
 class Command(CommandInterface):
 
     def init(self):
         self.parser = Parser("flux")
 
+    def setup(self) -> None:
+
+        if any(map(lambda x: x in self.line_args, ("-h", "--help"))):
+            self.print(self.banner())
+        super().setup()
 
     def run(self):
 
-        if len(self.line_args) == 1 or self.args.help:
-            self.description(random.choice([0, 1]))
+        if len(self.line_args) == 1:
+            self.print(self.banner())
 
 
-    def description(self, flip: bool = False):
-        emotes = [r"(╯°□°)╯︵ ┻━┻", r"(ノಠ益ಠ)ノ彡┻━┻", r"┬─┬ノ( º _ ºノ)", r"¯\_(ツ)_/¯"]
-        emote = random.choice(emotes) if flip else ""
-        title = self.colors.Fore.LIGHTBLACK_EX + r"""
-        {:^27}
-         _____ __    __ __  __  __ 
-        |   __|  |  |  |  |\  \/  /
-        |   _]|  |__|  |  | |    | 
-        |__|  |_____\_____//__/\__\ 
+    def banner(self):
+        emote = random.choice(EMOTES)
+        banner_width = 38
+        pad_len = max(0, (banner_width - len(emote)) // 2)
+        pad = " " * pad_len
+        emote = pad + emote
+        title = fr"""
+        {emote}
+         ____       _____ __    __ __  __  __ 
+        |    |___  |   __|  |  |  |  |\  \/  /
+        |___|    | |   _]|  |__|  |  | |    | 
+            |____| |__|  |_____\_____//__/\__\ 
                 
-        {:^27}
+        {'By @roysmanfo':^38}
         
-        """.format(emote, "By @roysmanfo")
-        self.print(title + self.colors.Fore.RESET)
+        """
+        return self.colors.Fore.LIGHTBLACK_EX + title + self.colors.Fore.RESET
 
 
