fd19d38384580a60d7f88d447d05560af174ce2e
roysmanfo <manforoys@gmail.com>
2024-08-16 16:59:02 +0200

    add: load both modules and pakages + reduce redundant code

--- flux/core/system/loader.py
+++ flux/core/system/loader.py
@@ -1,11 +1,13 @@
 import os
 import importlib
 from functools import lru_cache as _lru_cache
+from types import ModuleType
 from typing import Callable, Optional, TextIO
+
 from flux.core.system.system import System
 
 # List of directories to search for custom scripts/extensions
-custom_script_dirs = ["fpm"]
+custom_script_dirs = ["scripts", "fpm"]
 manager_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 # store up to 50 commands in the cache
@@ -19,14 +21,24 @@ def load_custom_script(script_name: str) -> Optional[Callable[[System, str, bool
     """
 
     for dir_name in custom_script_dirs:
-        script_path = os.path.join(dir_name, script_name + ".py")
-        if os.path.isfile(script_path):
-            try:
-                module = importlib.import_module(f"{dir_name}.{script_name}")
-                exec_command_class = getattr(module, "Command")
-                return exec_command_class
-            except (ImportError, AttributeError):
-                pass
+        script_path = os.path.join(manager_dir, "cmd", dir_name, script_name)
+        if os.path.exists(script_path):
+            if os.path.isfile(script_path + ".py"):
+                try:
+                    module = importlib.import_module(f"flux.core.cmd.{dir_name}.{script_name}.py")
+                    importlib.import_module()
+                    class_name = _get_entry_point(module)
+                    return getattr(module, class_name) if class_name else None
+                except ImportError:
+                    pass
+            elif os.path.isdir(script_path):
+                try:
+                    # in the case of 
+                    module = importlib.import_module(f"flux.core.cmd.{dir_name}.{script_name}.main.py")
+                    class_name = _get_entry_point(module)
+                    return getattr(module, class_name) if class_name else None
+                except ImportError:
+                    pass
     return None
 
 @_lru_cache(maxsize=max_cache_size, typed=False)
@@ -39,19 +51,26 @@ def load_builtin_script(script_name: str) -> Optional[Callable[[System, str, boo
     if os.path.exists(script_path) and os.path.isfile(script_path):
         try:
             module = importlib.import_module(f"flux.core.cmd.builtin.{script_name}", "flux")
-            try:
-                if hasattr(module, "ENTRY_POINT"):
-                    class_name = getattr(module, "ENTRY_POINT")
-                    if not hasattr(module, class_name):
-                        class_name = ""
-                else:
-                    class_name = "Command"
-            except AttributeError:
-                class_name = "Command"
-            finally:
-                return getattr(module, class_name) if class_name else None
+            class_name = _get_entry_point(module)
+            return getattr(module, class_name) if class_name else None
 
-        except (ImportError, AttributeError):
+        except ImportError:
             pass
 
     return None
+
+@_lru_cache(maxsize=max_cache_size, typed=False)
+def _get_entry_point(module: ModuleType) -> Optional[str]:
+    """
+    resolve the entry point for this command
+    
+    (usually it is the class Command, unles ENTRY_POINT is set)
+    """
+    class_name = "Command"
+    if hasattr(module, "ENTRY_POINT"):
+        class_name = getattr(module, "ENTRY_POINT")
+        if not class_name or not hasattr(module, class_name):
+            # either ENRY_POINT=""  or the specified class does not exist
+            class_name = None
+
+    return class_name
