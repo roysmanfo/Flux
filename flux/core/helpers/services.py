68dc98f8cbb8135eb9e3758f0f3730d9734def14
roysmanfo <manforoys@gmail.com>
2024-08-17 11:55:17 +0200

    add: flags and automatic service naming

--- flux/core/helpers/services.py
+++ flux/core/helpers/services.py
@@ -1,5 +1,6 @@
-from abc import ABC as _ABC, abstractmethod as _abstractmethod
-from typing import Callable
+from abc import ABCMeta as _ABCMeta, abstractmethod as _abstractmethod
+import os
+from typing import Callable, Optional
 
 from flux.core.system.system import System
 
@@ -12,7 +13,7 @@ def service_info_manager(func: Callable[[], None]):
     return wrapper
 
 
-class ServiceInterface(_ABC):
+class ServiceInterface(metaclass=_ABCMeta):
     """
     A service is a background program that is allowed to run as soon as the
     program starts 
@@ -21,11 +22,29 @@ class ServiceInterface(_ABC):
 
     def __init__(self,
                  system: System,
+                 name: Optional[str] = None,
                 ) -> None:
 
         self._running: bool = False
         self._enabled: bool = False
         self.system: System = system
+        self.name: str = name or os.path.splitext(os.path.basename(__file__))[0]
+
+
+
+    def __init_subclass__(cls) -> None:
+        cls._FLUX_SERVICE = True
+
+    @staticmethod
+    def _is_subclass(cls) -> bool:
+        cls_mro = [i.__name__ for i in cls.mro()[-3:]]
+        self_mro = [i.__name__ for i in ServiceInterface.mro()]
+        return cls_mro == self_mro
+
+    @staticmethod
+    def _is_subclass_instance(instance: object) -> bool:
+        _FLUX_SERVICE = getattr(instance, "_FLUX_SERVICE", None)
+        return _FLUX_SERVICE and isinstance(_FLUX_SERVICE, bool)
 
     @_abstractmethod
     @service_info_manager
