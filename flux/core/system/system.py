66719838d7ddd0c9ca53b92e21bbbe3de30fe66a
roysmanfo <manforoys@gmail.com>
2025-04-20 21:05:01 +0200

    feat: initialize command loader in System class

--- flux/core/system/system.py
+++ flux/core/system/system.py
@@ -1,4 +1,5 @@
 import os
+from flux.core.system import loader
 from flux.core.system.interrupts import InterruptHandler
 from flux.core.system.processes import Processes
 from flux.core.system.service_manager import Servicemanager
@@ -23,7 +24,8 @@ class System():
         self.variables: Variables = Variables()
         self.processes: Processes = Processes(self.interrupt_handler)
         self.service_manager: Servicemanager = Servicemanager(settings.syspaths.SERVICES_FILE)
-        
+        self.command_loader = loader
+
         self._init_reserved_variables()
         self._init_service_manager()
         
